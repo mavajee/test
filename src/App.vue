4bed53b84070c2df1acdf0659f03236678e95403
mavajee <mxv.vlasov@gmail.com>
2020-12-03 21:09:56 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <h1>Hello from v4 b003 43dh5542</h1>
+  <h1>Hello from v4 b003 5666</h1>
 </template>
 
 <script>
