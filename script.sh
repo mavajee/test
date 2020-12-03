8652d2c4499dcb1e6756f3d65a654371c4ba56ba
mavajee <mxv.vlasov@gmail.com>
2020-12-03 21:03:38 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-if [[ "$BRANCH" == "staging" || "$BRANCH" == "master"  ]] ; then
-  # Proceed with the build
-  exit 1;
-
-else
-  # Don't build
-  exit 0;
-fi
+echo script run;
+exit 0;
